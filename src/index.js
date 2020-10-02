db04aa1bf561f4b1ed8d1694c99488d6fe99105d
Chetan Jain <JAIN.CJ.CHETAN@gmail.com>
2020-10-02 16:10:36 +0530

    redux boilerplate and isUserLogged in integrated

--- src/index.js
+++ src/index.js
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    withRouter,
+} from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import "semantic-ui-css/semantic.min.css";
+import firebase from "./firebase";
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers/index";
+import { setUser } from "./actions";
+import Spinner from "./Spinner/Spinner";
 
+const store = createStore(rootReducer, composeWithDevTools());
+class Root extends Component {
+    componentWillMount() {
+        console.log("user");
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.props.setUser(user);
+                this.props.history.push("/");
+            }
+        });
+    }
+    render() {
+        return this.props.isLoading ? (
+            <Spinner />
+        ) : (
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/Login" component={Login} />
+                <Route path="/Register" component={Register} />
+            </Switch>
+        );
+    }
+}
+const mapStateToProps = (state) => ({
+    isLoading: state.user.isLoading,
+});
+const RootWithAuth = connect(mapStateToProps, { setUser })(withRouter(Root));
 ReactDOM.render(
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/Login" component={Login} />
-            <Route path="/Register" component={Register} />
-        </Switch>
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <RootWithAuth />
+        </Router>
+    </Provider>,
     document.getElementById("root")
 );
